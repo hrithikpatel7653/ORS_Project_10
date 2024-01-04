b375081c02ae1133291332ac4b14072bb3aacc20
hrithikpatel7653 <hrithikpatel7653@.com>
2024-01-04 18:09:00 +0530

    done

---
+++ projectORS/src/main/java/com/rays/common/message/MessageDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseDAOInt;
+
+/**
+ * Role DAO interface.
+ * @author Hrithik Patel 
+ */
+public interface MessageDAOInt extends BaseDAOInt<MessageDTO> {
+
+}
